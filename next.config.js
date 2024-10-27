fd6843f9462e1da09186a1a001ca0b9dcb214aff
Md Miftahul Islam <miftahulislam138@gmail.com>
2024-10-27 19:20:46 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["drhome.drhomeonline.net"],
+  },
+};
+
+module.exports = nextConfig;
---
+++ src/utils/staticCredential.js
@@ -0,0 +1,9 @@
+export const google_client_id =
+  "1000163153346-48ckm2uf7h70hmgn74kg1gq02sval158.apps.googleusercontent.com";
+export const fb_app_id = "380903914182154";
+export const appleLoginCredential = {
+  appleId: 'com.sixamtech.6amMart',
+  serviceId: 'com.sixamtech.sixamMartApp',
+  privateKey: 'MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgt6kR1nH1JEZ1Dd8Hk0cYHi5M5oW34o/me9B//D65lh6gCgYIKoZIzj0DAQehRANCAAQBCw9NAQZlT3lpp5D9IlpjSPJq9hSQrkNSvXil7qPHwHr5esLfs/qodgqRVxIY21mM/bk5VNYFbzDZR5ZCLZkG',
+  redirectURI:`https://6amtech.com/auth/apple/callback`
+}
